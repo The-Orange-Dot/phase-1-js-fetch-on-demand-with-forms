4bc01cd0195960db65f10635547cb1db2960c049
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-29 21:44:00 +0900

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+  //Add event listeners to capture form data and override a form's default behavior
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+
+    // console.log(event.target.children[1].value);
+  });
+  //Fetch data based on what the user types into that form
+  //Display that data on the page
+};
+
+document.addEventListener("DOMContentLoaded", init);
